b40d35fd9c0c7900640633b41f34cee8727fbd1a
lrhm <ilaimihar@gmail.com>
2022-07-12 01:33:18 +0200

    fixes

--- mate/__init__.py
+++ mate/__init__.py
@@ -4,14 +4,14 @@ from mate.model_checkpoint import ModelCheckpoint
 from mate.monitor import OptimizerMonitor
 
 # import mate.callback as callback
-from mate.metrics import DenormMSE, denorm_tanh_to_image, denorm_image_to_tanh_quantize
+# from mate.metrics import DenormMSE, denorm_tanh_to_image, denorm_image_to_tanh_quantize
+import mate.metrics
 
 __all__ = [
     "Cache",
     "Optimizer",
     "ModelCheckpoint",
     "OptimizerMonitor",
-    "DenormMSE",
-    "denorm_tanh_to_image",
-    "denorm_image_to_tanh_quantize",
+    "mate.metrics",
+    
 ]
--- mate/metrics.py
+++ mate/metrics.py
@@ -23,4 +23,17 @@ def denorm_tanh_to_image(x):
     return (x + 1) / 2
 
 
+class MovingSSIM(torchmetrics.SSIM):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+
+    def reshape(self, x):
+        # reshape to (batch*time, channel, height, width)
+        x = x.view(x.shape[0] * x.shape[1], *x.shape[2:])
+        return x
+
+    def update(self, preds: t.Tensor, target: t.Tensor) -> None:
+        return super().update(self.reshape(preds), self.reshape(target))
+
+
 __all__ = ["DenormMSE", "denorm_tanh_to_image", "denorm_image_to_tanh_quantize"]
