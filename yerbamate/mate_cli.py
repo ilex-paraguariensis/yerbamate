365b81275e7abb37e501ccaa199fa882994a9e42
GiulioZani <giulio.zani@gmail.com>
2022-07-12 10:02:14 +0200

    automatic CLI documentation

--- yerbamate/mate_cli.py
+++ yerbamate/mate_cli.py
@@ -1,4 +1,3 @@
-from argparse import ArgumentParser
 from .mate import Mate
 import inspect
 import ipdb
@@ -25,43 +24,85 @@ def get_methods_with_arguments(class_name):
     )
 
 
+def prettify_method(method, in_depth: bool = False):
+    def cleanup(ann):
+        return (
+            str(ann).replace("<class ", "").replace(">", "").replace("'", "")
+        )
+
+    def pretty(m):
+        return ",\n\t".join(
+            [
+                f"{m.name}: {cleanup(m.annotation)}"
+                + (f"={m.default}" if m.default != inspect._empty else "")
+                for m in method[1]
+            ]
+        )
+
+    source_code = inspect.getsource(getattr(Mate, method[0]))
+    description = (
+        source_code.split('"""')[1]
+        if '"""' in source_code and in_depth
+        else ""
+    )
+    return f" {method[0]}\n\t{pretty(method[1])}\n" + description
+
+
+def print_help(methods):
+    for method in methods:
+        print(prettify_method(method) + "\n")
+
+
 def main():
-    parser = ArgumentParser()
     methods = get_methods_with_arguments(Mate)
-    max_len = max(len(args) for args in methods)
+    max_len = max(len(args[1]) for args in methods)
+    """
     parser.add_argument(
         "action",
-        choices=tuple(method.replace("_", "-") for method, _ in methods),
+        choices=tuple(method.replace("_", "-") for method, _ in methods)
+        + ("help",),
         type=str,
         nargs="?",
     )
-    for i in range(max_len):
-        parser.add_argument(f"arg_{i}", type=str, nargs="?")
-    args = parser.parse_args()
-    args.action = args.action.replace("-", "_")
-    mate = Mate()
-    method_args_types = tuple(
-        tuple(param.annotation for param in params)
-        for method, params in methods
-        if method == args.action
-    )[0]
-    method_args_defaults = tuple(
-        tuple(param.default for param in params)
-        for method, params in methods
-        if method == args.action
-    )[0]
+    """
+    args = sys.argv[1:]
+    actions = tuple(method.replace("_", "-") for method, _ in methods) + (
+        "--help",
+        "-h",
+    )
 
-    raw_method_args = tuple(
-        args.__dict__[f"arg_{i}"] for i in range(len(method_args_types))
-    )
-    method_args = tuple(
-        (
-            method_type(raw_method_arg)
-            if (raw_method_arg is not None)
-            else method_default
+    if len(args) == 0 or not args[0] in actions or args[0] in ("--help", "-h"):
+        print_help(methods)
+    elif len(args) > 1 and args[1] in ("--help", "-h"):
+        print(
+            prettify_method(
+                [m for m in methods if m[0] == args[0]][0], in_depth=True
+            )
         )
-        for (method_type, method_default, raw_method_arg) in zip(
-            method_args_types, method_args_defaults, raw_method_args
+    # args.action = args.action.replace("-", "_")
+    else:
+        action = args[0]
+        mate = Mate()
+        method_args_types = tuple(
+            tuple(param.annotation for param in params)
+            for method, params in methods
+            if method == action
+        )[0]
+        method_args_defaults = tuple(
+            tuple(param.default for param in params)
+            for method, params in methods
+            if method == action
+        )[0]
+        raw_method_args = args[1:]
+        method_args = tuple(
+            (
+                method_type(raw_method_arg)
+                if (raw_method_arg is not None)
+                else method_default
+            )
+            for (method_type, method_default, raw_method_arg) in zip(
+                method_args_types, method_args_defaults, raw_method_args
+            )
         )
-    )
-    getattr(mate, args.action)(*method_args)
+        print(f"{method_args=}")
+        getattr(mate, action)(*method_args)
