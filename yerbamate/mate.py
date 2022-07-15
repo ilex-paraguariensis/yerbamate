923be88ecfd2938cf3377f7544533015ac2c5a11
lrhm <ilaimihar@gmail.com>
2022-07-15 07:26:31 +0200

    custom exec

--- yerbamate/mate.py
+++ yerbamate/mate.py
@@ -74,12 +74,17 @@ class Mate:
         return CustomLogger
 
     def __load_data_loader_class(self, data_loader_name: str):
-        data_class = (
-            f"{self.root_folder}.data_loaders.{data_loader_name}.data_loader"
-        )
+        data_class = f"{self.root_folder}.data_loaders.{data_loader_name}.data_loader"
         return __import__(
-            f"{data_class}", fromlist=["data_loader"],
+            f"{data_class}",
+            fromlist=["data_loader"],
         ).CustomDataModule
+
+    def __load_exec_function(self, exec_file: str):
+        return __import__(
+            f"{self.root_folder}.exec.{exec_file}",
+            fromlist=["exec"],
+        ).run
 
     def __set_save_path(self, model_name: str, params: str):
         self.save_path = os.path.join(
@@ -95,10 +100,7 @@ class Mate:
         self.__init_cache()
 
     def __override_params(self, params: Bunch):
-        if (
-            "override_params" in self.config
-            and self.config.override_params.enabled
-        ):
+        if "override_params" in self.config and self.config.override_params.enabled:
             for key, value in self.config.override_params.items():
                 if key == "enabled":
                     key = "override_params"
@@ -209,19 +211,13 @@ class Mate:
                 f'Are you sure you want to remove model "{model_name}"? (y/n)\n'
             )
         if action == "y":
-            os.system(
-                f"rm -r {os.path.join(self.root_folder, 'models', model_name)}"
-            )
+            os.system(f"rm -r {os.path.join(self.root_folder, 'models', model_name)}")
             print(f"Removed model {model_name}")
         else:
             print("Ok, exiting.")
 
     def list(self, folder: str):
-        print(
-            "\n".join(
-                tuple("\t" + str(m) for m in self.__list_packages(folder))
-            )
-        )
+        print("\n".join(tuple("\t" + str(m) for m in self.__list_packages(folder))))
 
     def clone(self, source_model: str, target_model: str):
         os.system(
@@ -237,9 +233,7 @@ class Mate:
             name.split("__")
             for name in os.listdir(os.path.join(self.root_folder, "snapshots"))
         ]
-        matching_snapshots = [
-            name for name in snapshot_names if name[0] == model_name
-        ]
+        matching_snapshots = [name for name in snapshot_names if name[0] == model_name]
         max_version_matching = (
             max([int(name[1]) for name in matching_snapshots])
             if len(matching_snapshots) > 0
@@ -263,17 +257,11 @@ class Mate:
         trainer.fit(model, datamodule=data_module)
 
     def train(self, model_name: str, params: str = "default"):
-        assert (
-            model_name in self.models
-        ), f'Model "{model_name}" does not exist.'
-        print(
-            f"Training model {model_name} with hyperparameters: {params}.json"
-        )
+        assert model_name in self.models, f'Model "{model_name}" does not exist.'
+        print(f"Training model {model_name} with hyperparameters: {params}.json")
 
         self.__set_save_path(model_name, params)
-        checkpoint_path = os.path.join(
-            self.save_path, "checkpoint", "last.ckpt"
-        )
+        checkpoint_path = os.path.join(self.save_path, "checkpoint", "last.ckpt")
         action = "go"
         if os.path.exists(checkpoint_path):
             while action not in ("y", "n", ""):
@@ -287,31 +275,21 @@ class Mate:
                 return
 
         # delete old checkpoints
-        os.system(
-            f"rm {os.path.join(self.save_path, 'checkpoint','optimizer*.pt')}"
-        )
-        os.system(
-            f"rm {os.path.join(self.save_path, 'checkpoint','scheduler*.pt')}"
-        )
+        os.system(f"rm {os.path.join(self.save_path, 'checkpoint','optimizer*.pt')}")
+        os.system(f"rm {os.path.join(self.save_path, 'checkpoint','scheduler*.pt')}")
 
         self.__fit(model_name, params)
 
     def test(self, model_name: str, params: str):
-        assert (
-            model_name in self.models
-        ), f'Model "{model_name}" does not exist.'
+        assert model_name in self.models, f'Model "{model_name}" does not exist.'
         params = "parameters" if params == "" or params == "None" else params
-        print(
-            f"Testing model {model_name} with hyperparameters: {params}.json"
-        )
+        print(f"Testing model {model_name} with hyperparameters: {params}.json")
 
         trainer, model, data_module = self.__get_trainer(model_name, params)
         trainer.test(model, datamodule=data_module)
 
     def restart(self, model_name: str, params: str):
-        assert (
-            model_name in self.models
-        ), f'Model "{model_name}" does not exist.'
+        assert model_name in self.models, f'Model "{model_name}" does not exist.'
         params = "parameters" if params == "" or params == "None" else params
         print(f"Restarting model {model_name} with parameters: {params}.json")
 
@@ -329,9 +307,7 @@ class Mate:
         installs a package
         """
         source_model_base_name = (
-            source_model.split(".")[-1]
-            if "." in source_model
-            else source_model
+            source_model.split(".")[-1] if "." in source_model else source_model
         )
         mate_dir = ".matedir"
         if not os.path.exists(mate_dir):
@@ -344,20 +320,21 @@ class Mate:
             os.path.join(source_model, destination_model)
         )
         old_params_files = [
-            os.path.join("hyperparameters", p)
-            for p in os.listdir("hyperparameters")
+            os.path.join("hyperparameters", p) for p in os.listdir("hyperparameters")
         ]
         for old_params_file in old_params_files:
             params_name = old_params_file.split(".")[0]
-            old_params = self.__read_hyperparameters(
-                destination_model, params_name
-            )
+            old_params = self.__read_hyperparameters(destination_model, params_name)
             old_params[source_model_base_name] = new_parameters
             with open(old_params_file, "w") as f:
                 json.dump(old_params, f)
 
-    def exec(self, model: str, file: str = ""):
-        pass
+    def exec(self, model: str, params: str, exec_file: str):
+        params = "parameters" if params == "" or params == "None" else params
+        print(f"Executing model {model} with result of: {params}")
+        _, model, _ = self.__get_trainer(model, params)
+
+        self.__load_exec_function(exec_file)(model)
 
     """ Import all submodules of a module, recursively, including subpackages
     :param package: package (name or actual module)
