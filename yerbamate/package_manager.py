0022fbe848f18a4bc1e148bb5adf23bb29cba61a
lrhm <ilaimihar@gmail.com>
2022-09-19 17:20:51 +0200

    refactor

--- yerbamate/package_manager.py
+++ yerbamate/package_manager.py
@@ -57,3 +57,51 @@ class PackageManager:
         # delete .mate_temp dir
         # os.system("rm -rf .mate_temp")
         pass
+
+
+class TorchPipModelPackage(PackageManager):
+    def __init__(
+        self,
+        package: Package,
+    ):
+        super().__init__(package=package)
+
+    def install(self):
+
+        self.clone_repo()
+        self.fix_import_bug()
+        self.copy_files()
+
+        # install requirements?
+        # pip install -r requirements.txt
+
+        self.post_install()
+
+    def clone_repo(self):
+
+        assert self.source == "git"
+
+        # Clone the repo
+        # git clone
+        temp_dir = ".mate_temp"
+        if os.path.exists(temp_dir):
+            shutil.rmtree(temp_dir)
+        os.mkdir(temp_dir)
+        os.system(f"git clone {self.url} {temp_dir}")
+
+    def fix_import_bug(self):
+        # change to relative import
+        #
+
+        pass
+
+    # copy to destination
+    def copy_files(self):
+        pass
+
+    # maybe generate a signature file for the package, so that we can check if the package is installed and what models are available
+    def post_install(self):
+
+        # delete .mate_temp dir
+        # os.system("rm -rf .mate_temp")
+        pass
