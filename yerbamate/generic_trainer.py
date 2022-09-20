7ed996c01538ef6da3c05e69d93eac6fc691e98a
lrhm <ilaimihar@gmail.com>
2022-09-20 18:43:55 +0200

    generic trainer class

---
+++ yerbamate/generic_trainer.py
@@ -0,0 +1,50 @@
+from .parse import Parser
+from .package import Package
+from yerbamate import parser
+from .bunch import Bunch
+from .trainer import Trainer
+import ipdb
+
+
+class GenericTrainer(Trainer):
+    def __init__(
+        self, params: Bunch, root_module, base_module, map_key_values, *kwargs
+    ):
+        super().__init__(params, *kwargs)
+
+        self.parser = Parser(
+            root_module=root_module,
+            base_module=base_module,
+            map_key_values=map_key_values,
+        )
+        self.install()
+
+    def install(self):
+
+        assert (
+            "pytorch_lightning_module" and "trainer" and "data" in self.params
+        ), "params must contain pytorch_lightning_module, trainer and data"
+
+        root = self.params.clone()
+        objects = self.parser.load_python_object(root)
+
+        self.objects = objects
+
+        # rename
+
+    def is_pl(self):
+        return True
+
+    def fit(self, *args, **kwargs):
+
+        self.parser.call_method(self.params.trainer, "fit", *args, **kwargs)
+
+    def test(self, *args, **kwargs):
+
+        self.parser.call_method(self.params.trainer, "test", *args, **kwargs)
+
+    def save(self, path: str):
+        pass
+
+    def load(self, path: str):
+        pass
