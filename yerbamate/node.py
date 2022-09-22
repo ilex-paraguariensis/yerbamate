58f6e3eb358eabb95bc7ffa715548c577b6f3d7c
lrhm <ilaimihar@gmail.com>
2022-09-22 04:51:36 +0200

    node update

--- yerbamate/node.py
+++ yerbamate/node.py
@@ -1,6 +1,8 @@
 from typing import Optional, Any, Callable, Type, Union
 import json
 
+import regex as re
+
 from .bunch import Bunch
 import random
 import ipdb
@@ -9,13 +11,16 @@ import ipdb
 class Node:
     object_key: Optional[str] = None
     _py_object: Optional[object] = None
+    _parent = None
 
-    def __init__(self, args, **kawrgs) -> None:
+    def __init__(self, args, parent=None, **kawrgs) -> None:
 
         if args is None:
             args = self._json()
         for key, val in args.items():
             setattr(self, key, val)
+
+        _parent = parent
 
         if self.object_key is None:
             self.object_key = "_rnd_" + "".join(  # it will start with
@@ -30,6 +35,19 @@ class Node:
         Node._base_module = base_module
         Node._root_module = root_module
         Node._key_value_map = key_value_map
+
+    def load_dynamic_objects(self):
+        for key, value in self.__dict__.items():
+            if type(value) == str and re.search(r"{.*}", value):
+                # get the key name
+                key_name = re.search(r"{.*}", value).group(0)[1:-1]
+                # replace the value
+                dynamic_object = value.replace(
+                    "{" + key_name + "}", Node._key_value_map[key_name]
+                )
+                setattr(self, key, dynamic_object)
+
+        # return object
 
     def post_object_creation(self):
         if "object_key" in self.__dict__:
@@ -58,10 +76,10 @@ class Node:
         if module == None:
             raise ModuleNotFoundError(f"module {module} not found")
 
-        if "class" in self:
-            module = getattr(module, getattr[self, "class"])
-        if "class_name" in self:
-            module = getattr(module, getattr[self, "class_name"])
+        if "class_name" in self.__dict__:
+            module = getattr(module, self.class_name)
+        # if "class_name" in self:
+        #     module = getattr(module, getattr[self, "class_name"])
 
         return module
 
@@ -134,18 +152,40 @@ class Node:
     def __load__(self, *args, **kwargs):
         pass
 
+    def __call__(self, *args, **kwargs):
+        if self._py_object != None:
+            return self._py_object
+        else:
+            super_dict = super().__dict__
+            return dict(
+                {key: val for key, val in super_dict.items() if not key.startswith("_")}
+            )
+
+    # def __dict__(self):
+    #     super_dict = super().__dict__
+    #     return dict({key: val for key, val in super_dict.items() if not key.startswith("_")})
+
 
 class NodeDict(Node):
     def __init__(self, args, **kawrgs) -> None:
-        assert type(args) == dict
+        assert type(args) == dict or type(args) == Bunch
         super().__init__(args, **kawrgs)
 
     def __load__(self, parent: Optional[Node] = None):
 
-        for key, val in self.items():
+        for key, val in self.__dict__.items():
+            val = load_node(val, parent=self)
             if isinstance(val, Node):
-                val.__load__(self, self)
+                val.__load__(self)
                 setattr(self, key, val())
+            elif isinstance(val, list):
+                for i, item in enumerate(val):
+                    item = load_node(item, parent=self)
+                    if isinstance(item, Node):
+                        item.__load__(self)
+                        val[i] = item()
+
+        self.load_dynamic_objects()
 
     def __call__(self, *args: Any, **kwds: Any) -> Any:
         return self.__dict__
@@ -229,6 +269,32 @@ class Object(Node):
         super().__init__(args)
 
 
+class Object(Node):
+    module: str = ""
+    class_name: str = ""
+    params: Bunch = Bunch({})  # param is actualy a dictnode
+
+    def __load__(self, parent: Optional[object] = None) -> object:
+
+        if self._py_object != None:
+            return self._py_object
+
+        module = self.load_module()
+
+        param_node = NodeDict(self.params)
+
+        param_node.__load__(self)
+
+        self._py_object = module(**param_node())
+
+        self.post_object_creation()
+
+        return self._py_object
+
+    def __init__(self, args: Optional[Bunch], parent: Optional[object] = None):
+        super().__init__(args, parent=parent)
+
+
 # only accepted as method_args inside objects
 class AnonMethodCall(Node):
     function: str = ""
@@ -260,8 +326,18 @@ node_types: list[Type] = [
     Object,
     MethodCall,
     ObjectReference,
-    # LRScheduler,
-    # Optimizers,
+    NodeDict,
     AnonMethodCall,
 ]
-SyntaxNode = Union[Object, MethodCall, ObjectReference, AnonMethodCall]
+SyntaxNode = Union[Object, MethodCall, ObjectReference, AnonMethodCall, NodeDict]
+
+
+def load_node(args, parent=None):
+    if isinstance(args, dict):
+        node: Optional[SyntaxNode] = None
+        for node_type in node_types:
+            if node_type.is_one(args):
+                node = node_type(args, parent=parent)
+                return node
+
+    return args
