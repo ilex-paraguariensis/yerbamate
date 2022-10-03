8661083defdac4925bcddbcf990a998d95c0d34d
lrhm <ilaimihar@gmail.com>
2022-10-03 23:52:50 +0200

    class metadata

--- packages/yerbamate/packages/metadata/package_metadata.py
+++ packages/yerbamate/packages/metadata/package_metadata.py
@@ -5,7 +5,7 @@ import sys, inspect
 from ..package import Package
 import ipdb
 
-from .utils import get_function_args
+from bombilla.utils import get_function_args
 
 
 class ModelMetadataGenerator:
@@ -20,20 +20,47 @@ class ModelMetadataGenerator:
         self.root_module = root_module
         self.type_module = type_module
         self.local_module = local_module
+        self.package_info = package_info
 
         self.module_path = os.path.join(
             self.root_module, self.type_module, self.local_module
         )
+        self.module = self.__get_local_module()
         self.module_files = os.listdir(self.module_path)
 
     def generate(self):
         ipdb.set_trace()
-        module = self.__get_local_module()
-        classes = self.__find_classes(module)
-        functions = self.__find_functions(module)
+
+        classes = self.__find_classes(self.module)
+
+        meta = [self.generate_class_metadata(klass) for klass in classes]
+
+        functions = self.__find_functions(self.module)
+
+        fun_meta = [self.generate_function_metadata(function) for function in functions]
+
         return {
-            "classes": classes,
-            "functions": functions,
+            "classes": meta,
+            "functions": fun_meta,
+        }
+
+    def generate_class_metadata(self, klass):
+
+        args, errors = get_function_args(klass[1].__init__)
+        return {
+            "class_name": klass[0],
+            "module": self.local_module,
+            "params": args,
+            "errors": errors,
+        }
+
+    def generate_function_metadata(self, function):
+        args, errors = get_function_args(function[1])
+        return {
+            "function_name": function[0],
+            "module": self.local_module,
+            "params": args,
+            "errors": errors,
         }
 
     def __get_local_module(self):
