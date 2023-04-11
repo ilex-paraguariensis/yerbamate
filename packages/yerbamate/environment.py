6799477ff262b997f0c297b7613bf5c6abffc937
oalee <oalee@proton.me>
2023-04-11 14:39:39 +0200

    minor updates

--- packages/yerbamate/environment.py
+++ packages/yerbamate/environment.py
@@ -8,10 +8,10 @@ import ipdb
 
 ENV_KEY = "env"
 
+
 class Environment(dict):
 
     def __init__(self):
-        
 
         # default restart, test to False
         self.restart = False
@@ -21,11 +21,12 @@ class Environment(dict):
         self.hparams = {}
 
         # set command to True
-        setattr(self, sys.argv[1], True)
-        
+        try:
+            setattr(self, sys.argv[1], True)
+        except:
+            pass
 
         self._root = self.__find_root()
-
 
         # parse python -m module train arg=1 arg2=2
         for arg in sys.argv:
@@ -36,11 +37,11 @@ class Environment(dict):
             setattr(self, key, value)
             self.hparams[key] = value
 
-        
         self._path = sys.argv[0]
         if "bin/mate" in self._path:
             self.name = os.path.join(*sys.argv[2:4])
-            self._path = os.path.join(self._root, "experiments", sys.argv[2],  sys.argv[3] + ".py")
+            self._path = os.path.join(
+                self._root, "experiments", sys.argv[2],  sys.argv[3] + ".py")
             # detect path to experiment
         else:
             self.name = self._path.split("/")[-2:]
@@ -67,19 +68,20 @@ class Environment(dict):
 
     def __find_root(self):
         # TODO, find root of the project
-        
+
         path = os.getcwd()
 
         if os.path.exists(os.path.join(path, "mate.json")):
             conf = json.load(open(os.path.join(path, "mate.json"), "r"))
 
             return os.path.join(path, conf["project"])
-       
+
         return None
 
     def __generate_experiment(self):
         # copies the experiment to the results folder
-        results =["results", "results_path", "results_dir", "save", "save_path", "save_dir"]
+        results = ["results", "results_path",
+                   "results_dir", "save", "save_path", "save_dir"]
         result = None
         for key in results:
             if key in self.env:
@@ -89,11 +91,11 @@ class Environment(dict):
             return
         if not os.path.exists(result):
             os.makedirs(result)
-        
+
         save_file = os.path.join(result, "experiment.py")
         # copy from self._path to save_file
         shutil.copyfile(self._path, save_file)
-       
+
         # ipdb.set_trace()
 
         if self.hparams != {}:
@@ -102,8 +104,6 @@ class Environment(dict):
             # overwrite if exists
             with open(params_file, "w") as f:
                 json.dump(dict(self.hparams), f, indent=4)
-
-
 
     def __set_env(self):
         mate_conf_path = os.path.join("mate.json")
@@ -123,7 +123,6 @@ class Environment(dict):
             with open(env_path, "w") as f:
                 json.dump(defualt_env, f, indent=4)
 
-            
         with open(env_path, "r") as f:
             try:
                 env = json.load(f)
@@ -143,33 +142,35 @@ class Environment(dict):
                     else:
                         env_not_found.append(key)
                         env[key] = value
-        
+
         if len(env_not_found) > 0:
-            print(f"Environment variables not found. Set them in env.json or in your shell.")
+            print(
+                f"Environment variables not found. Set them in env.json or in your shell.")
             for key in env_not_found:
                 desc = conf[ENV_KEY][key] if key in conf[ENV_KEY] else "Required environment variable"
                 print(f"{key} : {desc}")
 
             print("Exiting...")
             sys.exit(1)
-            
+
         # automatically append experiment name to results path
-        search_results = ["results", "results_path", "results_dir", "save", "save_path", "save_dir"]
+        search_results = ["auto_save"]
         for key in search_results:
             if key in env:
                 env[key] = os.path.join(env[key], *self.name.split("/"))
-                env["results"] = env[key]
+                # env["results"] = env[key]
                 break
             if os.environ.get(key, None) is not None and os.environ.get(key, None) != "":
                 env[key] = os.environ.get(key)
-                env["results"] = env[key]
+                # env["results"] = env[key]
                 break
-            
+
         if len(env) == 0:
-            print(f"results/save_dir/save environment variable is empty. Set it in env.json or in your shell.")
+            print(
+                f"results/save_dir/save environment variable is empty. Set it in env.json or in your shell.")
             print("Exiting...")
             sys.exit(1)
-        
+
         setattr(self, "env", env)
 
     def __getitem__(self, key):
@@ -180,12 +181,11 @@ class Environment(dict):
             res = os.environ.get(key, None)
 
             if res is None or res == "":
-                print(f"Environment variable {key} not found. Set it in env.json or in your shell.")
-                print("Exiting...") 
+                print(
+                    f"Environment variable {key} not found. Set it in env.json or in your shell.")
+                print("Exiting...")
                 sys.exit(1)
             else:
                 return res
-            
-        return super().__getitem__(key)
 
- 
+        return super().__getitem__(key)
